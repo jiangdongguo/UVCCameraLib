d31e662d51f497fa4ad349420f3c5a3cd7b5e20c
jiangdongguo <jiangdongguo@yy.com>
2021-03-16 12:37:14 +0800

    Usb camera ndk library for AndroidUSBCamera:https://github.com/jiangdongguo/AndroidUSBCamera

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Note
+
+ndk library for https://github.com/jiangdongguo/AndroidUSBCamera
+
+# Usage
+
+- 1. Download project and config sdk & NDK;
+- 2. Modify code and make libuvccamera module;
+- 3. Copy all so files to AndroidUSBCamera from lib directory;
+
+# Version
+
+- 1.0.0.20210316
+
+fix stopPreview Crash
---
+++ libuvccamera/src/main/obj/local/mips/objs/UVCCamera/Parameters.o.d
@@ -0,0 +1,69 @@
+./obj/local/mips/objs/UVCCamera/Parameters.o: \
+  jni/UVCCamera/Parameters.cpp jni/UVCCamera/Parameters.h \
+  jni/UVCCamera/libUVCCamera.h \
+  jni/libusb/android/jni/../../libusb\libusb.h \
+  jni/libuvc/android/jni/../../include/libuvc\libuvc.h \
+  jni/libuvc/android/jni/../../include\libuvc/libuvc_config.h \
+  jni/UVCCamera/..\utilbase.h jni/UVCCamera/../localdefines.h \
+  jni/UVCCamera/../rapidjson/include\rapidjson/rapidjson.h \
+  E:/Emvironment/Android/android-ndk-r14b-windows-x86_64/android-ndk-r14b/build//../sources/cxx-stl/system/include\cstdlib \
+  E:/Emvironment/Android/android-ndk-r14b-windows-x86_64/android-ndk-r14b/build//../sources/cxx-stl/system/include\cstring \
+  E:/Emvironment/Android/android-ndk-r14b-windows-x86_64/android-ndk-r14b/build//../sources/cxx-stl/system/include\cstddef \
+  E:/Emvironment/Android/android-ndk-r14b-windows-x86_64/android-ndk-r14b/build//../sources/cxx-stl/system/include\cassert \
+  jni/UVCCamera/../rapidjson/include\rapidjson/allocators.h \
+  jni/UVCCamera/../rapidjson/include\rapidjson/encodings.h \
+  jni/UVCCamera/../rapidjson/include\rapidjson/stringbuffer.h \
+  jni/UVCCamera/../rapidjson/include\rapidjson/internal/stack.h \
+  jni/UVCCamera/../rapidjson/include\rapidjson/writer.h \
+  jni/UVCCamera/../rapidjson/include\rapidjson/internal/strfunc.h \
+  jni/UVCCamera/../rapidjson/include\rapidjson/internal/dtoa.h \
+  jni/UVCCamera/../rapidjson/include\rapidjson/internal/itoa.h \
+  E:/Emvironment/Android/android-ndk-r14b-windows-x86_64/android-ndk-r14b/build//../sources/cxx-stl/system/include\new \
+  jni/libuvc/android/jni/../../include\libuvc/libuvc_internal.h \
+  jni/libuvc/android/jni/../../include\utlist.h
+
+jni/UVCCamera/Parameters.h:
+
+jni/UVCCamera/libUVCCamera.h:
+
+jni/libusb/android/jni/../../libusb\libusb.h:
+
+jni/libuvc/android/jni/../../include/libuvc\libuvc.h:
+
+jni/libuvc/android/jni/../../include\libuvc/libuvc_config.h:
+
+jni/UVCCamera/..\utilbase.h:
+
+jni/UVCCamera/../localdefines.h:
+
+jni/UVCCamera/../rapidjson/include\rapidjson/rapidjson.h:
+
+E:/Emvironment/Android/android-ndk-r14b-windows-x86_64/android-ndk-r14b/build//../sources/cxx-stl/system/include\cstdlib:
+
+E:/Emvironment/Android/android-ndk-r14b-windows-x86_64/android-ndk-r14b/build//../sources/cxx-stl/system/include\cstring:
+
+E:/Emvironment/Android/android-ndk-r14b-windows-x86_64/android-ndk-r14b/build//../sources/cxx-stl/system/include\cstddef:
+
+E:/Emvironment/Android/android-ndk-r14b-windows-x86_64/android-ndk-r14b/build//../sources/cxx-stl/system/include\cassert:
+
+jni/UVCCamera/../rapidjson/include\rapidjson/allocators.h:
+
+jni/UVCCamera/../rapidjson/include\rapidjson/encodings.h:
+
+jni/UVCCamera/../rapidjson/include\rapidjson/stringbuffer.h:
+
+jni/UVCCamera/../rapidjson/include\rapidjson/internal/stack.h:
+
+jni/UVCCamera/../rapidjson/include\rapidjson/writer.h:
+
+jni/UVCCamera/../rapidjson/include\rapidjson/internal/strfunc.h:
+
+jni/UVCCamera/../rapidjson/include\rapidjson/internal/dtoa.h:
+
+jni/UVCCamera/../rapidjson/include\rapidjson/internal/itoa.h:
+
+E:/Emvironment/Android/android-ndk-r14b-windows-x86_64/android-ndk-r14b/build//../sources/cxx-stl/system/include\new:
+
+jni/libuvc/android/jni/../../include\libuvc/libuvc_internal.h:
+
+jni/libuvc/android/jni/../../include\utlist.h:
