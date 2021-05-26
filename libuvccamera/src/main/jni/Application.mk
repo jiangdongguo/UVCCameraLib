0d556ae8f5dba8e5b1da07106a150f17e7a72175
Oliver Graf <oliver.graf@teamviewer.com>
2021-05-26 07:42:30 +0200

    make build with latest ndk

--- libuvccamera/src/main/jni/Application.mk
+++ libuvccamera/src/main/jni/Application.mk
@@ -26,7 +26,7 @@
 # Note: Supporting GCC on NDK is already deprecated and GCC will be removed from NDK soon.
 #NDK_TOOLCHAIN_VERSION := 4.9
 
-APP_PLATFORM := android-14
+APP_PLATFORM := android-21
 APP_ABI :=arm64-v8a armeabi-v7a x86 x86_64
 #APP_OPTIM := debug
 APP_OPTIM := release
